412689cd7a3772c065f7b64c6dd6fdc7bb4f8d56
innerviewer <innerviewerr@gmail.com>
2024-03-14 06:55:09 +0100

    (Staging) Intermediate commit.

---
+++ main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by innerviewer on 2024-03-14.
+//
+
+#include <Utils/Debug.h>
+
+int main(int argc, char *argv[])
+{
+    SR_LOG("Hello, World from SpaRcle DA Template!");
+
+    return 0;
+}
