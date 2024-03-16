3757b33745fc319427bea37c761e4ce4bcc38f08
innerviewer <innerviewerr@gmail.com>
2024-03-16 12:13:12 +0100

    (CMake) Fixed build.

--- main.cpp
+++ main.cpp
@@ -3,10 +3,21 @@
 //
 
 #include <Utils/Debug.h>
+#include <Utils/Platform/Platform.h>
+#include <Utils/Types/Thread.h>
+#include <Utils/Locale/Encoding.h>
 
-int main(int argc, char *argv[])
-{
-    SR_LOG("Hello, World from SpaRcle DA Template!");
+int main(int argc, char *argv[]) {
+    auto&& applicationPath = SR_PLATFORM_NS::GetApplicationPath().GetFolder();
 
+    SR_UTILS_NS::Debug::Instance().Init(applicationPath, true, SR_UTILS_NS::Debug::Theme::Dark);
+    SR_HTYPES_NS::Thread::Factory::Instance().SetMainThread();
+
+    SR_UTILS_NS::Locale::SetLocale();
+
+    SR_LOG("Starting application...");
+
+    SR_LOG("Exiting application...");
+    SR_UTILS_NS::GetSingletonManager()->DestroyAll();
     return 0;
 }
